546d4ab4c05efef53bca34ae3d6e8818e7ea883b
lbarberiscanoni <hllbck7@gmail.com>
2015-11-18 20:56:34 -0500

    added the announcements page

--- scripts/announcements.js
+++ scripts/announcements.js
@@ -9,6 +9,6 @@ $(document).ready(function() {
         var timeStamp = new Date(timeStamp);
         var timeStamp = timeStamp.toString().split("GMT")[0];
         var timeStamp = timeStamp.split("2015")[0] + " @ " + timeStamp.split("2015")[1];
-        $("<div class='row'><div class='col-md-4'></div><div class='col-md-4'><h3>" + announcement.title + "</h3><h4>" + timeStamp + "</h4></h3><p>" + announcement.content + "</p></div><div class='col-md-4'></div></div>").insertAfter("#pastAnnouncements h2");
+        $("#announcements").append("<div class='row'><div class='col-md-4'></div><div class='col-md-4'><h3>" + announcement.title + "</h3><h4>" + timeStamp + "</h4></h3><p>" + announcement.content + "</p></div><div class='col-md-4'></div></div>");
     });
 });
