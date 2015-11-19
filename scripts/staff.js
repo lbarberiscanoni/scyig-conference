8cb63789784abf41f6be20955405b2f157eea14d
lbarberiscanoni <hllbck7@gmail.com>
2015-11-18 19:35:53 -0500

    moved the jquery call into https

--- scripts/staff.js
+++ scripts/staff.js
@@ -1,2 +1,6 @@
+var listOfQuestions = new Firebase("https://yig-bill-tracker.firebaseio.com/researchQuestions_clone");
 $(document).ready(function() {
+    $("#submit").click(function() {
+        alert("hello");
+    });
 });
